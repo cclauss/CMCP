0572250107816b1097626ce571c935c4991e098c
cclauss <cclauss@bluewin.ch>
2016-05-01 11:21:07 +0200

    str.format(**dict)

--- main.py
+++ main.py
@@ -19,33 +19,29 @@ Desinged for use with Enigma's mappings
 """
 
 
-def readconfig():
-    global Config
-    config = open(os.getcwd()+"/config.conf")
-    Config = json.load(config)
-    config.close()
+def read_config(rel_path="config.conf"):
+    with open(os.path.join(os.getcwd(), rel_path)) as in_file:
+        return json.load(in_file)
 
 def deobfuscate():
     """Deobfuscates Minecraft using the setting supplied in the setting file. Then decompiles it"""
 
+    print("""Config is targeting version {MC Verson} with a side of: {Side}
+Using Enigma Mappings with there version being: {Mapping Version}
+The editor files will be built for: {Editor}""".format(**Config))
     mcVersion = Config["MC Verson"]
     side = Config["Side"]
-    editor = Config["Editor"]
-    mapVersion = Config["Mapping Version"]
-    print("""Config is targeting version %s with a side of: %s
-Using Enigma Mappings with there version being: %s
-The editor files will be built for: %s""" % (mcVersion, side, mapVersion, editor))
-    deobf.downloadMCplusLibs(side,mcVersion)
+    deobf.downloadMCplusLibs(side, mcVersion)
     deobf.downloadDecompAndDeobf(Config)
-    deobf.downloadMappings(mcVersion,mapVersion,side)
-    deobf.deobf(mcVersion,side,Config)
-    deobf.decompile(side,Config)
-    #deobf.editor(editor)
+    deobf.downloadMappings(mcVersion, Config["Mapping Version"], side)
+    deobf.deobf(mcVersion, side, Config)
+    deobf.decompile(side, Config)
+    # deobf.editor(Config["Editor"])
 
 
 if __name__ == "__main__":
     print(HEADER)
     print("Reading Config")
-    readconfig()
-    ssjb.registerTask("deobf",deobfuscate)
+    Config = read_config()
+    ssjb.registerTask("deobf", deobfuscate)
     ssjb.run()
